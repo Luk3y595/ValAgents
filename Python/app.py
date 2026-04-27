6a9bd1ba77dee5aea5c1d7eae946c9e3b34fda99
Luk3y595 <mankey595595@gmail.com>
2026-04-28 09:19:31 +1200

    added python to make the queries to show better when on website

--- Python/app.py
+++ Python/app.py
@@ -13,3 +13,21 @@ def get_db():
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
     return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
