d9fe3012f4922fd0435fc29cb186e2e3241ba563
Luk3y595 <mankey595595@gmail.com>
2026-06-07 10:04:42 +1200

    added the query for the agents

--- app.py
+++ app.py
@@ -25,17 +25,21 @@ def home():
 def agent():
     # agent page with all the info
     sql = """
-                SELECT AgentInfo.agent_id,AgentInfo.name,
-                Role.role,BestMaps.best_maps,
-                BestMaps.map_image_url,WorstMaps.worst_maps,WorstMaps.map_image_url,
-                AgentInfo.signature_ability_one,AgentInfo.signature_ability_one_description,
-                AgentInfo.signature_ability_two,AgentInfo.signature_ability_two_description,AgentInfo.ability_one,
-                AgentInfo.ability_one_description,AgentInfo.ability_two,AgentInfo.ability_two_description,
-                AgentInfo.ability_three,AgentInfo.ability_three_description,AgentInfo.ultimate,AgentInfo.ultimate_description
-                FROM AgentInfo
-                JOIN Role ON Role.role_id=AgentInfo.role_id
-                JOIN BestMaps ON BestMaps.best_map_id=AgentInfo.best_map_id
-                JOIN WorstMaps ON WorstMaps.worst_map_id=AgentInfo.worst_map_id
+            SELECT AgentInfo.agent_id,
+            AgentInfo.name,Role.role,Flag.flag_img_url,
+            BestMaps.best_maps,BestMaps.map_image_url,WorstMaps.worst_maps,WorstMaps.map_image_url,
+            AgentInfo.signature_ability_one,AgentInfo.signature_ability_one_description,
+            AgentInfo.signature_ability_two,AgentInfo.signature_ability_two_description,
+            AgentInfo.ability_one,AgentInfo.ability_one_description,AgentInfo.ability_two,
+            AgentInfo.ability_two_description,AgentInfo.ability_three,AgentInfo.ability_three_description,
+            AgentInfo.ultimate,AgentInfo.ultimate_description,AgentInfo.country_of_origin,AgentInfo.ability_img_url,
+            AgentInfo.ability_two_img_url,AgentInfo.ability_three_img_url,AgentInfo.ability_four_img_url,
+            AgentInfo.ability_five_img_url,AgentInfo.ultimate_img_url,AgentInfo.fullart_img_id
+            FROM AgentInfo
+            JOIN Role ON Role.role_id=AgentInfo.role_id
+            JOIN BestMaps ON BestMaps.best_map_id=AgentInfo.best_map_id
+            JOIN WorstMaps ON WorstMaps.worst_map_id=AgentInfo.worst_map_id
+            JOIN Flag ON Flag.flag_id=AgentInfo.flag_id
 """
     results = query_db(sql)
     return render_template('agent.html', agent=results)
